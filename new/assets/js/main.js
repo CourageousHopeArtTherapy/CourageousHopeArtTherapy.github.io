85f3c8f7da7f4db8413b2b1a39091b06aebb0a72
CourageousHopeArtTherapy <adam.matthew.bell@gmail.com>
2022-10-02 15:07:59 +0100

    add image gallery, wording updates

--- new/assets/js/main.js
+++ new/assets/js/main.js
@@ -58,6 +58,51 @@
         });
     });
 
+    startImageTransition();
+ 
+    function startImageTransition() {
+        const images = document.getElementsByClassName("image-gallery");
+
+        for (var i = 0; i < images.length; ++i) {
+            images[i].style.opacity = 1;
+        }
+
+        var top = 1; 
+        var cur = images.length - 1;
+
+        setInterval(changeImage, 6000);
+
+        async function changeImage() { 
+            const nextImage = (1 + cur) % images.length;
+
+            images[cur].style.zIndex = top + 1;
+            images[nextImage].style.zIndex = top;
+
+            await transition();
+
+            images[cur].style.zIndex = top; 
+            images[nextImage].style.zIndex = top + 1; 
+            top = top + 1; 
+            images[cur].style.opacity = 1;
+            cur = nextImage; 
+        }
+
+        function transition() {
+            return new Promise(function(resolve, reject) {
+                const del = 0.01;
+                const id = setInterval(changeOpacity, 10);
+
+                function changeOpacity() {
+                    images[cur].style.opacity -= del;
+                    if (images[cur].style.opacity <= 0) {
+                        clearInterval(id);
+                        resolve();
+                    }
+                }
+            });
+        }
+    }
+
     "use strict";
 
 }) ();
